dd5be77c43df7c316549696bf13bcf8b43e5bb12
wsapcf <wsapcf@gmail.com>
2024-04-25 12:48:24 +0000

    Finalize zod config

---  env/index.ts
+++  env/index.ts
@@ -4,10 +4,19 @@ import {z} from "zod"
 const envSchema = z.object({
     DATABASE_URL: z.string(),
     PORT: z.number(),
-    NODE_ENV = z.enum("development", "testing", "production").default("production"),
+    NODE_ENV: z.enum(["development", "testing", "production"]).default("production"),
 
 
 })
 
-export const env = envSchema.parse(process.env)
+ const _env = envSchema.safeParse(process.env)
+
+ if(_env.success === false){
+    console.error("Invalid environment configuration at: ", _env.error.format())
+    throw new Error(
+        "Invalid environment configuration"
+    )
+ }
+
+ export const env = _env.data
 
